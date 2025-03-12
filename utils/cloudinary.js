6278d9c8838239f6a38593518035ad1a36e7ed3c
shahariaz <shahariaz.info@gmail.com>
2025-03-13 02:22:02 +0600

    feat: wirte utils for uploading media to the cloudinary

---
+++ utils/cloudinary.js
@@ -0,0 +1,34 @@
+import { v2 as cloudinary } from "cloudinary";
+import dotenv from "dotenv";
+dotenv.config({});
+
+cloudinary.config({
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_NAME,
+});
+
+export const uploadMedia = async (file) => {
+  try {
+    const uploadResponse = await cloudinary.uploader.upload(file, {
+      resource_type: "auto",
+    });
+    return uploadResponse;
+  } catch (error) {
+    console.log(`Error uploading image: ${error}`);
+  }
+};
+export const delteMediaFromCLoudinary = async (publicId) => {
+  try {
+    await cloudinary.uploader.destroy(publicId);
+  } catch (error) {
+    console.log(`Error deleting image: ${error}`);
+  }
+};
+export const deleteVideoFromCloudinary = async (publicId) => {
+  try {
+    await cloudinary.uploader.destroy(publicId, { resource_type: "video" });
+  } catch (error) {
+    console.log(`Error deleting video: ${error}`);
+  }
+};
