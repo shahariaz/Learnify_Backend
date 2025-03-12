ed99c59afafbd20cedad7c8fa11b088dc6887672
shahariaz <shahariaz.info@gmail.com>
2025-03-12 19:03:15 +0600

    feat: Implement some security middleware

--- index.js
+++ index.js
@@ -2,8 +2,25 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import morgan from "morgan";
+import rateLimit from "express-rate-limit";
+import helmet from "helmet";
+import hpp from "hpp";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 const app = express();
+
 const PORT = process.env.PORT;
+
+//Global Rate Limiting
+const Limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: "Too many requests from this IP, please try again in 15 minutes",
+});
+//Security Middleware
+app.use("/api", Limiter);
+app.use(helmet());
+app.use(hpp());
 
 app.get("/", (req, res) => {
   res.send("Welcome to the home page");
@@ -17,7 +34,7 @@ if (process.env.NODE_ENV === "development") {
 //Body Parser Middleware
 app.use(express.json({ limit: "100kb" }));
 app.use(express.urlencoded({ extended: true, limit: "100kb" }));
-
+app.use(cookieParser());
 //Global Error Handler
 app.use((err, _, res, __) => {
   console.error(err.stack);
@@ -27,6 +44,24 @@ app.use((err, _, res, __) => {
     ...(process.env.NODE_ENV === "development" && { stack: err.stack }),
   });
 });
+//Cors Configuration
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "X-Requested-With",
+      "Accept",
+      "Origin",
+      "X-HTTP-Method-Override",
+      "device-remember-token",
+      "Access-Control-Allow-Origin",
+    ],
+  })
+);
 
 //API Routes
 
