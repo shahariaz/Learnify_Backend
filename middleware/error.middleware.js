476e59ddbef8c921ec2f43e8fa36795b2ff343db
shahariaz <shahariaz.info@gmail.com>
2025-03-17 01:51:34 +0600

    feat: write custom error class

--- middleware/error.middleware.js
+++ middleware/error.middleware.js
@@ -1,3 +1,13 @@
+export class ApiError extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+    this.status = `${statusCode}.startsWith('4') ? 'fail': 'error'`;
+    this.isOperational = true;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
 export const catchAsync = (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
